9af2b41fab8753c7b451d7d3b3a59e10a1bdac49
Dylan <dcooksonmlec@student.unimelb.edu.au>
2022-09-19 22:48:57 +1000

    Implemented rook movement

---
+++ Assets/Scripts/Rook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rook : MonoBehaviour {
+
+    [SerializeField] float range;
+    [SerializeField] float speed;
+    int direction = 1;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        if (transform.localPosition.z > range) {
+            direction = -1;
+        } else if (transform.localPosition.z < -range) {
+            direction = 1;
+        }
+        transform.Translate(Vector3.forward * speed * direction * Time.deltaTime);
+    }
+
+}
