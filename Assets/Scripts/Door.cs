dd0c3cfbb8f3daa7acd20b5f4622ee33a10ed268
Dylan <dcooksonmlec@student.unimelb.edu.au>
2022-10-04 10:40:03 +1100

    Completed door logic

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -13,7 +13,6 @@ public class Door : MonoBehaviour, Interaction {
     }
 
     public void InteractWith() {
-        Debug.Log("HELLO");
         if (hasKey) {
             anim.Play();
             hasKey = false;
