5d7f0bf14a81e9e7c2bd404b0476aa90ef841350
Thomas Robertson <thomasrobertson24@gmail.com>
2022-10-18 02:04:38 +1100

    fix bad floor

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -13,12 +13,12 @@ public class Floor : MonoBehaviour
     [SerializeField][Min(1)] private int tileCountX = 1;
     [SerializeField][Min(1)] private int tileCountY = 1 ;
 
-    [Header("Rippling")]
-    [SerializeField][Min(0)] private int lifetime = 5;
-    [SerializeField][Min(0)] private float spread = 0.2f;
+    // [Header("Rippling")]
+    // [SerializeField][Min(0)] private int lifetime = 5;
+    // [SerializeField][Min(0)] private float spread = 0.2f;
 
-    [Header("Player")]
-    [SerializeField] private GameObject player = null;
+    // [Header("Player")]
+    // [SerializeField] private GameObject player = null;
 
     private GameObject[,] tiles;
     // private List<Vector3> ripples = new List<Vector3>();
@@ -31,13 +31,13 @@ public class Floor : MonoBehaviour
         {
             // tile.GetComponent<Renderer>().sharedMaterial.SetVector("_Ripples", new Vector2[] {origin});
             tile.GetComponent<Renderer>().sharedMaterial.SetVector("_Ripples", rippleOrigin);
-            tile.GetComponent<Renderer>().sharedMaterial.SetFloat("_Spread", spread);
+            // tile.GetComponent<Renderer>().sharedMaterial.SetFloat("_Spread", spread);
         }
         AddRipple(new Vector2(2, 2));
     }
 
-    void Update()
-    {
+    // void Update()
+    // {
         // ensure old ripples disappear
         // List<Vector3> remainingRipples = new List<Vector3>();
         // foreach (Vector3 ripple in ripples)
@@ -48,19 +48,19 @@ public class Floor : MonoBehaviour
         // }
         // ripples = remainingRipples;
 
-        Debug.Log(player.transform.position.x + ", " + player.transform.position.z);
-        foreach (GameObject tile in tiles)
-        {
-            tile.GetComponent<Renderer>().sharedMaterial.SetVector("_Ripples", new Vector2(player.transform.position.x, player.transform.position.z));
-        }
-        Debug.Log(lifetime);
+        // Debug.Log(player.transform.position.x + ", " + player.transform.position.z);
+        // foreach (GameObject tile in tiles)
+        // {
+        //     tile.GetComponent<Renderer>().sharedMaterial.SetVector("_Ripples", new Vector2(player.transform.position.x, player.transform.position.z));
+        // }
+        // Debug.Log(lifetime);
 
         // rippleOrigin += new Vector2(1, 1) * (1f * Time.deltaTime);
         // foreach (GameObject tile in tiles)
         // {
         //     tile.GetComponent<Renderer>().sharedMaterial.SetVector("_Ripples", rippleOrigin);
         // }
-    }
+    // }
 
     private void GenerateFloorTiles(int tileCountX, int tileCountY)
     {
@@ -79,34 +79,40 @@ public class Floor : MonoBehaviour
     private GameObject GenerateTile(float tileSize, int x, int y, Material material)
     {
         // TODO: add tileSize square size transformation
-        GameObject tile = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
-        // tile.transform. = transform;
+        // CHECK THE ROTATIONS ON THIS OBJECT
+        // GameObject tile = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
 
-        Mesh mesh = new Mesh();
-        tile.AddComponent<MeshFilter>().mesh = mesh;
-        tile.AddComponent<MeshRenderer>().material = material;
+        // REMOVE THIS LATER
+        GameObject tile = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        tile.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+        tile.transform.position = tile.transform.position + new Vector3(x, 0, y);
+        tile.GetComponent<Renderer>().material = material;
 
-        Vector3[] vertices = new Vector3[4];
-        Vector2[] uvs = new Vector2[vertices.Length];
+        // Mesh mesh = new Mesh();
+        // tile.AddComponent<MeshFilter>().mesh = mesh;
+        // tile.AddComponent<MeshRenderer>().material = material;
 
-        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
-        vertices[1] = new Vector3(x * tileSize, 0, (y + 1) * tileSize);
-        vertices[2] = new Vector3((x + 1) * tileSize, 0, y * tileSize);
-        vertices[3] = new Vector3((x + 1) * tileSize, 0, (y + 1) * tileSize);
+        // Vector3[] vertices = new Vector3[4];
+        // Vector2[] uvs = new Vector2[vertices.Length];
 
-        for (int i = 0; i < uvs.Length; i++)
-        {
-            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-        }
+        // vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
+        // vertices[1] = new Vector3(x * tileSize, 0, (y + 1) * tileSize);
+        // vertices[2] = new Vector3((x + 1) * tileSize, 0, y * tileSize);
+        // vertices[3] = new Vector3((x + 1) * tileSize, 0, (y + 1) * tileSize);
 
-        int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
+        // for (int i = 0; i < uvs.Length; i++)
+        // {
+        //     uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+        // }
 
-        mesh.vertices = vertices;
-        mesh.triangles = tris;
-        mesh.uv = uvs;
+        // int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
-        tile.AddComponent<BoxCollider>();
-        tile.transform.position = tile.transform.position + new Vector3(0.5f, 0, 0.5f);
+        // mesh.vertices = vertices;
+        // mesh.triangles = tris;
+        // mesh.uv = uvs;
+
+        // tile.AddComponent<MeshCollider>();
+        // tile.transform.position = tile.transform.position + new Vector3(0.5f, 0, 0.5f);
 
         return tile;
     }
