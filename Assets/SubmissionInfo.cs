b38e0d2b305b82160992d9db4d2d3933750a01f9
Kaiyi Sun <kaiyis1@student.unimelb.edu.au>
2022-10-18 23:57:53 +1100

    youtube video

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -34,7 +34,7 @@ public static class SubmissionInfo
   // By the gameplay video milestone deadline this should be a direct link
   // to a YouTube video upload containing your video. Ensure "Made for kids"
   // is turned off in the video settings. 
-  public static readonly string GameplayVideo = "https://youtube.com/...";
+  public static readonly string GameplayVideo = "https://youtu.be/U2HQTEUbJNs";
 
   public readonly struct TeamMember
   {
