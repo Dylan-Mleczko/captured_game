be6ce36ac86d5bb633b4319a10d9dfbe5c915f1f
Thomas Robertson <thomasrobertson24@gmail.com>
2022-11-03 10:55:01 +1100

    brightness of data manager carries

--- Assets/Scripts/Brightness.cs
+++ Assets/Scripts/Brightness.cs
@@ -1,3 +1,5 @@
+// Credit to https://www.youtube.com/watch?time_continue=161&v=XiJ-kb-NvV4&feature=emb_title&ab_channel=JTAGames
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +8,23 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class Brightness : MonoBehaviour
 {
-    public Slider brightnessSlider;
+    public GameObject brightnessSlider;
     public PostProcessProfile brightness;
-    public PostProcessLayer layer;
+    public GameObject layer;
     AutoExposure exposure;
 
     void Start() {
         brightness.TryGetSettings(out exposure);
+    }
+
+    void Update() {
+        if (brightnessSlider == null) {
+            brightnessSlider = GameObject.Find("BrightnessSlider");
+        }
+
+        if (layer == null) {
+            layer = GameObject.Find("Main Camera");
+        }
     }
 
     public void AdjustBrightness(float value) {
